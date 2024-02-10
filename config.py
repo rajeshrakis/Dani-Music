c762d7ef4d9f593b807f9b758e479c900e1ae21d
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2024-02-10 15:27:40 +0530

    config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/HeartBeat_Offi")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/https://t.me/HeartBeat_Muzic")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/HeartBeat_Muzic")
 
 # Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
