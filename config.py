7a2e58cc988242a051506631db283352b5a7404b
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2024-02-17 14:24:40 +0530

    config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/rajeshrakis/HeartBeat-Music",
+    "https://github.com/rajeshrakis/Dani-Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
